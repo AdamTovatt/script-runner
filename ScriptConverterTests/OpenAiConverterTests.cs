7f74e457afa56101c82adee022808f2b7a942fe3
Adam Tovatt <sporedenstore@hotmail.com>
2023-07-23 23:07:44 +0200

    complete converter tests

--- ScriptConverterTests/OpenAiConverterTests.cs
+++ ScriptConverterTests/OpenAiConverterTests.cs
@@ -44,6 +44,18 @@ namespace ScriptConverterTests
             Assert.IsNotNull(compileResult.CompiledAssembly);
 
             Function function = OpenAiScriptConverter.GetAsFunction(compileResult);
+
+            Assert.AreEqual("This is a test script that multiplies two numbers and returns the result.", function.Description);
+            Assert.AreEqual("TestScript.Run", function.Name);
+            Assert.IsNotNull(function.Parameters);
+            Assert.IsNotNull(function.Parameters.Parameters);
+            Assert.AreEqual(3, function.Parameters.Parameters.Count);
+            Assert.AreEqual("factor1", function.Parameters.Parameters[0].Name);
+            Assert.AreEqual("The first number to multiply", function.Parameters.Parameters[0].Description);
+            Assert.AreEqual("factor2", function.Parameters.Parameters[1].Name);
+            Assert.AreEqual("The second number to multiply", function.Parameters.Parameters[1].Description);
+            Assert.AreEqual("userName", function.Parameters.Parameters[2].Name);
+            Assert.AreEqual("The username of the user", function.Parameters.Parameters[2].Description);
         }
     }
 }
