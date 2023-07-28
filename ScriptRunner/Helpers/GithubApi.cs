b3e9503a7395fe4e94964bbe5600670bb717b47f
Adam Tovatt <sporedenstore@hotmail.com>
2023-07-28 14:08:13 +0200

    start to add github support

---
+++ ScriptRunner/Helpers/GithubApi.cs
@@ -0,0 +1,41 @@
+﻿using Octokit;
+
+namespace ScriptRunner.Helpers
+{
+    public class GithubApi : IGithubApi
+    {
+        private GitHubClient client;
+        private string owner;
+        private string repoName;
+        private string branch;
+
+        public GithubApi(string githubToken, string productHeaderValue, string owner, string repoName, string branch)
+        {
+            client = new GitHubClient(new ProductHeaderValue(productHeaderValue));
+            client.Credentials = new Credentials(githubToken);
+            this.owner = owner;
+            this.repoName = repoName;
+            this.branch = branch;
+        }
+
+        public async Task CreateScript(string name, string code)
+        {
+            string filePath = $"{name}.cs";
+            await client.Repository.Content.CreateFile(owner, repoName, filePath, new CreateFileRequest($"Created {filePath}", code, branch));
+        }
+
+        public async Task UpdateScript(string name, string code)
+        {
+            string filePath = $"{name}.cs";
+            try
+            {
+                IReadOnlyList<RepositoryContent>? fileDetails = await client.Repository.Content.GetAllContentsByRef(owner, repoName, filePath, branch);
+                await client.Repository.Content.UpdateFile(owner, repoName, filePath, new UpdateFileRequest($"Updated {filePath}", code, fileDetails.First().Sha));
+            }
+            catch
+            {
+                await CreateScript(name, code);
+            }
+        }
+    }
+}
---
+++ ScriptRunner/Helpers/IGithubApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptRunner.Helpers
+{
+    public interface IGithubApi
+    {
+        public Task CreateScript(string name, string code);
+        public Task UpdateScript(string name, string code);
+    }
+}
