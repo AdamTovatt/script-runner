378bbc539809857a9e1d38f7577324dc1a86cf9d
Adam Tovatt <sporedenstore@hotmail.com>
2023-08-15 00:35:19 +0200

    small change in cloudinary script

--- SkippyBackend/PrecompiledScripts/UploadSvgToCloudinaryScript.cs
+++ SkippyBackend/PrecompiledScripts/UploadSvgToCloudinaryScript.cs
@@ -10,17 +10,17 @@ namespace SkippyBackend.PrecompiledScripts
         public UploadSvgToCloudinaryScript(ScriptContext context) : base(context) { }
 
         [ScriptStart]
-        [Parameter("svgName", "The name of the svg file. The name of the image." )]
+        [Parameter("svgName", "The name of the svg file. The name of the image.")]
         [AllowedRoles("admin")]
         public async Task<string> UploadSvgToCloudinary(string svgName)
         {
             try
             {
-                string input = await Context.Conversation.Input.GetAsync<string>("Please provide the content of the svg file as a string in the chat");
+                string input = (await Context.Conversation.Input.GetAsync<string>("Please provide the content of the svg file as a string in the chat"))!;
 
                 return await CloudinaryHelper.Instance.UploadImageAsync(svgName, Encoding.UTF8.GetBytes(input));
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 return $"Error when uploading svg: {exception.Message}";
             }
