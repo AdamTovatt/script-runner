2aeae22d3d2dcbaf0c56cbaa3a43bd877d61fe92
Adam Tovatt <sporedenstore@hotmail.com>
2023-09-06 15:35:33 +0200

    improve send text file script

--- SkippyBackend/PrecompiledScripts/SendTextFileScript.cs
+++ SkippyBackend/PrecompiledScripts/SendTextFileScript.cs
@@ -12,9 +12,11 @@ namespace SkippyBackend.PrecompiledScripts
         [ScriptStart]
         [Parameter("textContent", "The text content of the file to send. ")]
         [Parameter("fileName", "The name of the file to send. Can be made up to be descriptive of the content. ")]
-        public void SendTextFile(string textContent, string fileName)
+        public string SendTextFile(string textContent, string fileName)
         {
+            Context.Conversation.AddAssistantMessage("[text file]");
             Context.Conversation.Communicator.InvokeOnFileWasSent(this, Encoding.UTF8.GetBytes(textContent), ContentType.File, fileName);
+            return "A text file was sent in the chat";
         }
     }
 }
