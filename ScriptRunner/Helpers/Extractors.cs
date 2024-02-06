229fe672c0a48ac8275c8936a690432e3127b3e2
Adam Tovatt <sporedenstore@hotmail.com>
2024-02-06 16:01:52 +0100

    add extractors helper for custom extractors

---
+++ ScriptRunner/Helpers/Extractors.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace ScriptRunner.Helpers
+{
+    public class Extractors
+    {
+        /// <summary>
+        /// Will extract ints from strings by removing all non digit characters
+        /// </summary>
+        public static Func<string, object?> SimpleIntExtractor => (input) =>
+        {
+            if (string.IsNullOrEmpty(input))
+                return null;
+
+            StringBuilder cleanedInput = new StringBuilder();
+
+            foreach (char character in input)
+            {
+                if (char.IsDigit(character))
+                    cleanedInput.Append(character);
+            }
+
+            if (int.TryParse(cleanedInput.ToString(), out int result))
+                return result;
+
+            return null;
+        };
+    }
+}
